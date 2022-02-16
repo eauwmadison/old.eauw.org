fe042ba09ae2e16894f8b7481e3c7977f293cceb
Declan Dally <ddally@wisc.edu>
2022-02-16 13:32:55 -0600

    Corrected updated fellowship dates & shrunk fellowship link image size

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ function fellowshipPopup() {
   vex.dialog.defaultOptions.showCloseButton = true;
 
   vex.dialog.open({          
-    unsafeMessage: `<h1>Fellowship applications are now open!</h1><p>Application deadline Friday, February 18th.</p>`, 
+    unsafeMessage: `<h1>Fellowship applications are now open!</h1><p>Application deadline Friday, February 20th.</p>`, 
     callback: function (value) {
       if (value)
         window.location.href = "https://eauw.org/apply";
